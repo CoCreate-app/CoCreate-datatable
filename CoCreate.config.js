a76edd3be0399048704d4c15becec8a6063043d5
Frank Pagan <frank@cocreate.app>
2021-02-13 00:40:02 +0000

    clean tree

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -9,7 +9,7 @@ module.exports = {
     sources: [{
             path: "./docs/index.html",
             collection: "files",
-            document_id: "",
+            document_id: "6025f32d7a78a543a77c9222",
             key: "html",
             data:{
                 name: "Datatable Doc",
@@ -19,10 +19,10 @@ module.exports = {
    
     crud: [{
             collection: "routes",
-            document_id: "",
+            document_id: "6025f32d7a78a543a77c9221",
             data:{
                 collection: "files",
-                document_id: "",
+                document_id: "6025f32d7a78a543a77c9222",
                 name: "html",
                 domains: ["cocreate.app", "server.cocreate.app", "ws.cocreate.app"],
                 route: "/docs/datatable",
